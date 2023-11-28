8543a89c384393c047836a6cc92bed0946ac61d6
Tomáš Kvapil <tomaskvapil5@gmail.com>
2023-11-28 20:06:25 +0100

    pck_pokoje_terminu

--- database/packages/pck_pokoje_terminu.sql
+++ database/packages/pck_pokoje_terminu.sql
@@ -26,27 +26,31 @@ CREATE OR REPLACE PACKAGE BODY pck_pokoje_terminu AS
         p_pokoj_id IN OUT POKOJE_TERMINU.POKOJ_ID%TYPE,
         o_result OUT CLOB
     ) IS
+        v_exists NUMBER;
     BEGIN
-        IF p_termin_id IS NULL THEN
-            INSERT INTO POKOJE_TERMINU (CELKOVY_POCET_POKOJU, POCET_OBSAZENYCH_POKOJU, POKOJ_ID, TERMIN_ID)
-            VALUES (p_celkovy_pocet_pokoju, p_pocet_obsazenych_pokoju, p_pokoj_id, p_termin_id)
-            RETURNING TERMIN_ID, POKOJ_ID INTO p_termin_id, p_pokoj_id;
-            o_result := '{ "status": "OK", "message": "Nový záznam byl úspěně vytvořen." }';
-        ELSE
-            UPDATE POKOJE_TERMINU
-            SET CELKOVY_POCET_POKOJU = p_celkovy_pocet_pokoju,
-                POCET_OBSAZENYCH_POKOJU = p_pocet_obsazenych_pokoju
-            WHERE TERMIN_ID = p_termin_id and POKOJ_ID = p_pokoj_id;
-            IF SQL%ROWCOUNT = 0 THEN
-                o_result := '{ "status": "error", "message": "Chyba: Záznam s daným ID nebyl nalezen." }';
-            ELSE
-                o_result := '{ "status": "OK", "message": "Záznam byl úspěně aktualizován." }';
-            END IF;
-        END IF;
+        SELECT COUNT(*)
+    INTO v_exists
+    FROM POKOJE_TERMINU
+    WHERE TERMIN_ID = p_termin_id AND POKOJ_ID = p_pokoj_id;
+
+    IF v_exists > 0 THEN
+        UPDATE POKOJE_TERMINU
+        SET CELKOVY_POCET_POKOJU = p_celkovy_pocet_pokoju,
+            POCET_OBSAZENYCH_POKOJU = p_pocet_obsazenych_pokoju
+        WHERE TERMIN_ID = p_termin_id AND POKOJ_ID = p_pokoj_id;
+
+        o_result := '{ "status": "OK", "message": "Záznam byl úspěně aktualizován." }';
+    ELSE
+        INSERT INTO POKOJE_TERMINU (CELKOVY_POCET_POKOJU, POCET_OBSAZENYCH_POKOJU, POKOJ_ID, TERMIN_ID)
+        VALUES (p_celkovy_pocet_pokoju, p_pocet_obsazenych_pokoju, p_pokoj_id, p_termin_id)
+        RETURNING TERMIN_ID, POKOJ_ID INTO p_termin_id, p_pokoj_id;
+
+        o_result := '{ "status": "OK", "message": "Nový záznam byl úspěně vytvořen." }';
+    END IF;
     EXCEPTION
-        WHEN OTHERS THEN
+    WHEN OTHERS THEN
             o_result := '{ "status": "error", "message": "Došlo k chybě při zpracování: ' || REPLACE(SQLERRM, '"', '\"') || '" }';
-    END manage_pokoje_terminu;
+END manage_pokoje_terminu;
 
     PROCEDURE delete_pokoje_terminu(
         p_termin_id IN POKOJE_TERMINU.TERMIN_ID%TYPE,
--- database/packages/pck_security.sql
+++ database/packages/pck_security.sql
@@ -14,3 +14,64 @@ CREATE OR REPLACE PACKAGE BODY security AS
 
 END security;
 /
+
+CREATE OR REPLACE FUNCTION hash_password (str IN VARCHAR2)
+RETURN VARCHAR2
+IS v_checksum VARCHAR2(32);
+BEGIN
+v_checksum := LOWER( RAWTOHEX( UTL_RAW.CAST_TO_RAW(
+sys.dbms_obfuscation_toolkit.md5(input_string => str) ) ) );
+RETURN v_checksum;
+EXCEPTION WHEN NO_DATA_FOUND THEN NULL;
+WHEN OTHERS THEN
+RAISE;
+END hash_password;
+/
+GRANT EXECUTE ON hash_password TO PUBLIC;
+
+CREATE OR REPLACE PROCEDURE login(
+    p_usr_name IN VARCHAR2,
+    p_usr_pwd IN VARCHAR2,
+    p_result OUT CLOB,
+    p_usr_id OUT NUMBER
+) AS
+    v_id NUMBER(38); 
+    v_hashed_pwd VARCHAR2(64);
+BEGIN
+    v_hashed_pwd := hash_password(p_usr_pwd);
+
+    SELECT PRIHLASOVACI_UDAJE_ID INTO v_id
+    FROM PRIHLASOVACI_UDAJE
+    WHERE JMENO = p_usr_name
+    AND HESLO = v_hashed_pwd;
+    
+    IF SQL%FOUND THEN
+        p_usr_id := v_id;
+        p_result := '{ "status": "OK", "message": "Login successful." }';
+    ELSE
+        p_result := '{ "status": "error", "message": "Invalid username or password." }';
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        p_result := '{ "status": "error", "message": "Invalid username or password." }';
+    WHEN OTHERS THEN
+        p_result := '{ "status": "error", "message": "An unexpected error occurred: ' || SQLERRM || '" }';
+END login;
+
+
+
+
+
+create or replace
+PACKAGE PCK_SECURITY AS
+
+PROCEDURE edit_user(p_usr_id NUMBER,
+p_usr_name VARCHAR2, p_usr_pwd VARCHAR2,
+p_valid_from VARCHAR2, p_valid_till VARCHAR2,
+p_result OUT CLOB);
+
+PROCEDURE login(p_usr_name VARCHAR2, p_usr_pwd
+VARCHAR2,
+p_result OUT CLOB, p_usr_id OUT NUMBER);
+
+END PCK_SECURITY;
